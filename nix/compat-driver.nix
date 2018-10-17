c4d1611c4376e4e53ea8e4fe88879a6b88d14f28
Will Fancher <elvishjerricco@gmail.com>
2018-10-16 20:20:28 -0400

    Don't null the ghc attr

--- nix/compat-driver.nix
+++ nix/compat-driver.nix
@@ -4,7 +4,7 @@ lib.fix (hsPkgs: pkgs.callPackage "${pkgs.path}/pkgs/development/haskell-modules
   haskellLib = pkgs.haskell.lib;
   inherit ghc;
   buildHaskellPackages = hsPkgs;
-  compilerConfig = _: _: lib.mapAttrs (_: _: null) plan.compiler.packages;
+  compilerConfig = _: _: lib.mapAttrs (_: _: null) (builtins.removeAttrs plan.compiler.packages ["ghc"]);
   initialPackages = self: _:
     lib.mapAttrs
       (_: pkg: self.callPackage ./driver.nix { expr = lib.fix pkg; inherit lib; })
