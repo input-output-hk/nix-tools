9f16fc72c036d7b8664011f06d67d2f08fe8199f
Moritz Angermann <moritz.angermann@gmail.com>
2019-08-18 13:38:58 +0200

    Adds ci

---
+++ ci.nix
@@ -0,0 +1,18 @@
+builtins.mapAttrs (k: _v:
+  let
+    url = "https://github.com/NixOS/nixpkgs/archive/2255f292063ccbe184ff8f9b35ce475c04d5ae69.tar.gz";
+    pkgs = import (builtins.fetchTarball url) { system = k; };
+  in
+  pkgs.recurseIntoAttrs {
+    # These two attributes will appear in your job for each platform.
+    nix-tools = pkgs.runCommand "nix-tools-via-cabal-install" {
+      buildInputs = with pkgs; [ ghc cabal-install zlib.dev ];
+    } ''
+        cabal new-build
+    '';
+  }
+) {
+  x86_64-linux = {};
+  # Uncomment to test build on macOS too
+  # x86_64-darwin = {};
+}
