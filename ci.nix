5c3903da7a1cf221a3b11e44fb4aa687b62c6659
Moritz Angermann <moritz.angermann@gmail.com>
2019-08-18 15:58:24 +0200

    use haskell.nix

--- ci.nix
+++ ci.nix
@@ -1,18 +1,16 @@
 builtins.mapAttrs (k: _v:
   let
+    haskell-nix = builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/71716e4b216c421a8827d2642b97572afb9efd36.tar.gz";
     url = "https://github.com/NixOS/nixpkgs/archive/2255f292063ccbe184ff8f9b35ce475c04d5ae69.tar.gz";
-    pkgs = import (builtins.fetchTarball url) { system = k; };
+    pkgs = import (builtins.fetchTarball url) {
+      system = k;
+      overlays = import (haskell-nix + ./overlays);
+      config = import (haskell-nix + ./config.nix);
+    };
   in
   pkgs.recurseIntoAttrs {
     # These two attributes will appear in your job for each platform.
-    nix-tools = pkgs.runCommand "nix-tools-via-cabal-install" {
-      buildInputs = with pkgs; [ ghc cabal-install zlib.dev ];
-    } ''
-        export HOME=$TMP
-        mkdir $HOME/.cabal
-        cabal new-update
-        cabal new-build
-    '';
+    nix-tools = pkgs.haskell-nix.cabalProject { src = ./.; };
   }
 ) {
   x86_64-linux = {};
