23d2ffc85a3764138626fc477dd19dd5df54ccad
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2019-09-16 21:43:34 +1200

    Move nix1 and nix2 into .buildkite

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -18,11 +18,11 @@ cabal new-configure
 echo
 echo "+++ Run stable version of plan-to-nix"
 nix build -f https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz nix-tools -o nt
-./nt/bin/plan-to-nix --output nix1 --plan-json dist-newstyle/cache/plan.json
+./nt/bin/plan-to-nix --output .buildkite/nix1 --plan-json dist-newstyle/cache/plan.json
 
 echo
 echo "+++ Build project"
-nix build -f nix1 nix-tools.components.exes --no-link
+nix build -f .buildkite/nix1 nix-tools.components.exes --no-link
 
 echo
 echo "+++ Run tests"
@@ -42,20 +42,20 @@ echo "+++ Run plan-to-nix again"
 # https://github.com/input-output-hk/haskell.nix/issues/57
 rm -f .nix-tools.cache
 
-nix build -f nix1 nix-tools.components.exes.plan-to-nix
-./result/bin/plan-to-nix --output nix2 --plan-json dist-newstyle/cache/plan.json
+nix build -f .buildkite/nix1 nix-tools.components.exes.plan-to-nix
+./result/bin/plan-to-nix --output .buildkite/nix2 --plan-json dist-newstyle/cache/plan.json
 
 echo
 echo "+++ Build project"
 
-nix build -f nix2 nix-tools.components.exes --no-link
+nix build -f .buildkite/nix2 nix-tools.components.exes --no-link
 
 echo
 echo "--- Test index file truncation"
 
 find /nix/store/ -name "*-00-index.tar.gz" -exec nix-store --delete {} \;
 nix build -f test/truncate-index.nix --no-link \
-    --arg nix-tools-path ./nix2  \
+    --arg nix-tools-path ./.buildkite/nix2  \
     --argstr index-state "$index_state" \
     --argstr hash "$expected_hash" \
     indexTruncated
