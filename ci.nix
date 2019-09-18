13b8a3ffc03a562af6c55c5542cef5282918518e
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2019-09-18 23:05:12 +1200

    Bump haskell.nix in ci.nix

--- ci.nix
+++ ci.nix
@@ -1,6 +1,6 @@
 builtins.mapAttrs (k: _v:
   let
-    haskell-nix = builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/83e4d87e6e31662f0a731cb6cf0d0fb9126c7687.tar.gz";
+    haskell-nix = builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/160ecb6fd7eaa203c4939f79cdc5b5e2b24dd71e.tar.gz";
     url = "https://github.com/NixOS/nixpkgs/archive/2255f292063ccbe184ff8f9b35ce475c04d5ae69.tar.gz";
     pkgs = import (builtins.fetchTarball url) {
       system = k;
