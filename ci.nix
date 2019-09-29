a6dc4e4ea150b9659bb44cc2a6cea611c550602b
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2019-09-29 22:22:36 +1300

    Use newer haskell.nix in ci.nix

--- ci.nix
+++ ci.nix
@@ -1,6 +1,6 @@
 builtins.mapAttrs (k: _v:
   let
-    pkgs = import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/ee945efd4cb22e2cb5c58dce501312562d8ac960.tar.gz") {
+    pkgs = import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/57d58888d91f978fccd8648a05fe79dff71d2384.tar.gz") {
         nixpkgs = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/2255f292063ccbe184ff8f9b35ce475c04d5ae69.tar.gz";
         nixpkgsArgs = { system = k; };
       };
